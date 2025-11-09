00092ab6c2809f355b19459aa6a5f28af8e40560
hussam essa <hussamessa@Mac.home>
2025-11-09 16:44:22 +0100

    teams landing page

---
+++ learning-exams/f1-q.js
@@ -0,0 +1,17 @@
+
+const cards = document.getElementsByClassName("card");
+const body = document.getElementById("body");
+
+const teams = ["ferrari", "mercedes", "redbull"];
+
+for (var i = 0; i < cards.length; i++) {
+  const team = teams[i];
+
+  cards[i].addEventListener("mouseenter", () => {
+    body.className = team; // add class
+  });
+
+  cards[i].addEventListener("mouseleave", () => {
+    body.className = ""; // remove when leaving
+  });
+}
