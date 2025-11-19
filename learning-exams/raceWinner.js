a6a9efe176725cb2e67ad49fb8cac6ed3d21c2e6
Hossam <94597991+HMI20@users.noreply.github.com>
2025-11-19 23:25:36 +0100

    fixing bugs

--- learning-exams/raceWinner.js
+++ learning-exams/raceWinner.js
@@ -1,8 +1,4 @@
 function main (){
-    const driversData = [
-        { name: "Max Verstappen", score: 0.98, strategy: strategies[0], team: car [0] },
-        { name: "Luis Hamelton", score: 0.96, strategy: strategies[1],team: car [1] },
-        { name: "Lando Noras", score: 0.97,strategy: strategies[2],team: car [2] }  ];
     const car = [
         { team: "Red Bull", score: 0.92 },
         { team: "Mercedes", score: 0.95 },
@@ -14,43 +10,46 @@ function main (){
         hard:   { base: 0.90, wearRate: 0.005 }   };
 
     const strategies = [
-        [ {tire:"soft",laps: 5}, {tire:"midium",laps: 10}, {tire:"hard",laps:15}  ],
-        [ {tire:"midium",laps:9} , {tire:"midium",laps: 9}, {tire:"hard",laps: 12}],
-        [ {tire:"midium" ,laps: 12}, {tire:"hard",laps: 18} ]    ];
+        [ {tire:tires.soft,laps: 5}, {tire:tires.medium,laps: 10}, {tire:tires.hard,laps:15}  ],
+        [ {tire:tires.medium,laps:9} , {tire:tires.medium,laps: 9}, {tire:tires.hard,laps: 12}],
+        [ {tire:tires.medium ,laps: 12}, {tire:tires.hard,laps: 18} ]    ];
 
+    const driversData = [
+        { name: "Max Verstappen", score: 0.98, strategy: strategies[0], team: car [0] },
+        { name: "Luis Hamelton", score: 0.96, strategy: strategies[1],team: car [1] },
+        { name: "Lando Noras", score: 0.97,strategy: strategies[2],team: car [2] }  ];
     const idealLap = 100; //in seconds
     const pitStopTime = 20;
-    console.log("the winner is "+ getWinner(driversData,idealLap,pitStopTime,tires))
+    console.log("the winner is "+ getWinner(driversData,idealLap,pitStopTime))
 }
 
-function getWinner(driversData, idealLap, pitStopTime, tires) {
+function getWinner(driversData, idealLap, pitStopTime) {
     let winner = " ";
     let min = Infinity;
-
-    for (let i = 0; i < driversData.length; i++) {
-        const teamScore = (driversData[i].score * 0.6 + driversData[i].team * 0.4);
+    debugger ;for (let i = 0; i < driversData.length; i++) {
+        const teamScore = (driversData[i].score * 0.6 + driversData[i].team.score * 0.4);
         let raceTime = 0;
 
-        for( let j = 0; j< driversData[i].strategy.length; j++){
-
+        for (let j = 0; j < driversData[i].strategy.length; j++) {
 
             for (let lapNumber = 1; lapNumber <= driversData[i].strategy[j].laps; lapNumber++) {
-                const tiresBase = tires[driversData[i].strategy[j].tire].base;
-                const tiresWear = tires[driversData[i].strategy[j].tire].wearRate;
+                const tiresBase = driversData[i].strategy[j].tire.base;
+                const tiresWear = driversData[i].strategy[j].tire.wearRate;
                 const lapTime = idealLap + (100 * (1 - (teamScore * (tiresBase - (tiresWear * lapNumber)))))
                 raceTime = raceTime + lapTime;
 
             }
-            
+
         }
-        const pitStops = (driversData[i].strategy.length)-1;
-        const raceTimeInSec = raceTime + (pitStops*pitStopTime);
+        const pitStops = (driversData[i].strategy.length) - 1;
+        const raceTimeInSec = raceTime + (pitStops * pitStopTime);
         const raceTimeInMin = raceTimeInSec / 60;
-            console.log("Driver " + driversData[i].name + " race time " + raceTimeInMin);
-            if (raceTimeInMin < min) {
-                min = raceTimeInMin;
-                winner = driversData[i].name;
-            }
+        console.log("Driver " + driversData[i].name + " race time " + raceTimeInMin);
+        if (raceTimeInMin < min) {
+            min = raceTimeInMin;
+            winner = driversData[i].name;
+        }
     }
     return winner;
 }
+main();
