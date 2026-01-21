acac7b11328e1025756bdd4f12d9a49bd98b7730
Hossam <94597991+HMI20@users.noreply.github.com>
2026-01-21 23:28:50 +0100

    expand driver and team options, and refine race simulation logic.

--- learning-exams/raceWinner.js
+++ learning-exams/raceWinner.js
@@ -1,16 +1,16 @@
-let calculateLapTimeLoss = (performanceScore, tire, lapNumber)=> {
-    const tirePerformance = tire.base - tire.wearRate * lapNumber; 
+let calculateLapTimeLoss = (performanceScore, tire, lapNumber) => {
+    const tirePerformance = tire.base - tire.wearRate * lapNumber;
     const performanceFactor = 1 - performanceScore * tirePerformance;
     return 100 * performanceFactor;
 }
 
-let getStrategy = (lapNumber, strategies)=>{
+let getStrategy = (lapNumber, strategies) => {
     let lapsCount = 0;
     let isNew = false;
-    for (let i = 0; i < strategies.length; i++){
+    for (let i = 0; i < strategies.length; i++) {
         lapsCount = lapsCount + strategies[i].laps;
-        if (lapNumber > lapsCount){
-            if (lapNumber - lapsCount == 1){
+        if (lapNumber > lapsCount) {
+            if (lapNumber - lapsCount == 1) {
                 isNew = true;
             }
         } else {
@@ -21,17 +21,17 @@ let getStrategy = (lapNumber, strategies)=>{
 
 let getLapRecord = (drivers, idealLapTime) => {
     const raceTime = [];
-    for (let lapNumber = 1; lapNumber <= 30; lapNumber++){
+    for (let lapNumber = 1; lapNumber <= 30; lapNumber++) {
         for (let i = 0; i < drivers.length; i++) {
             const performanceScore = drivers[i].score * 0.6 + drivers[i].car.score * 0.4;
             let lapTime = 0;
             const currentStrategy = getStrategy(lapNumber, drivers[i].strategy);
             lapTime = idealLapTime + calculateLapTimeLoss(performanceScore, currentStrategy.currentStrategy.tire, lapNumber);
-            
+
             if (raceTime[i] == undefined) raceTime[i] = 0;
-            
-            if (currentStrategy.isNew){
-                raceTime[i] = raceTime[i] + lapTime + 20; 
+
+            if (currentStrategy.isNew) {
+                raceTime[i] = raceTime[i] + lapTime + 20;
             } else {
                 raceTime[i] = raceTime[i] + lapTime;
             }
@@ -40,7 +40,7 @@ let getLapRecord = (drivers, idealLapTime) => {
     }
 }
 
-let getRaceWinner = (drivers, idealLapTime, pitStopDuration) =>{
+let getRaceWinner = (drivers, idealLapTime, pitStopDuration) => {
     let winner = '';
     let fastestRaceTime = Infinity;
     for (let i = 0; i < drivers.length; i++) {
@@ -65,7 +65,7 @@ let getRaceWinner = (drivers, idealLapTime, pitStopDuration) =>{
 let describeStrategy = (strategy) => {
     let strategyText = ' ';
     for (let i = 0; i < strategy.length; i++) {
-        if (i === strategy.length - 1){
+        if (i === strategy.length - 1) {
             strategyText = strategyText + strategy[i].tire.type + ' for ' + strategy[i].laps + ' laps ';
         } else {
             strategyText = strategyText + strategy[i].tire.type + ' for ' + strategy[i].laps + ' laps → ';
@@ -88,18 +88,47 @@ let startRaceSimulation = () => {
     }
 }
 const validator = (selectedDrivers, currentDriver) => {
-  
-  let alreadyExists = selectedDrivers.some(d => (currentDriver.name == d.name && currentDriver.car.team == d.car.team && describeStrategy(currentDriver.strategy)== describeStrategy(d.strategy)));
+
+    let alreadyExists = selectedDrivers.some(d => (currentDriver.name == d.name && currentDriver.car.team == d.car.team && describeStrategy(currentDriver.strategy) == describeStrategy(d.strategy)));
     return alreadyExists
 }
 let addDriver = () => {
     const drivers = [
-        { name: 'Max Verstappen', score: 0.98 },
-        { name: 'Lewis Hamilton', score: 0.96 },
+        { name: 'Max Verstappen', score: 0.99 },
+        { name: 'Isack Hadjar', score: 0.85 },
+        { name: 'George Russell', score: 0.94 },
+        { name: 'Andrea Kimi Antonelli', score: 0.88 },
+        { name: 'Charles Leclerc', score: 0.96 },
+        { name: 'Lewis Hamilton', score: 0.97 },
+        { name: 'Lando Norris', score: 0.96 },
+        { name: 'Oscar Piastri', score: 0.94 },
+        { name: 'Fernando Alonso', score: 0.95 },
+        { name: 'Lance Stroll', score: 0.88 },
+        { name: 'Pierre Gasly', score: 0.90 },
+        { name: 'Franco Colapinto', score: 0.86 },
+        { name: 'Alex Albon', score: 0.91 },
+        { name: 'Carlos Sainz Jr.', score: 0.93 },
+        { name: 'Liam Lawson', score: 0.88 },
+        { name: 'Arvid Lindblad', score: 0.82 },
+        { name: 'Gabriel Bortoleto', score: 0.84 },
+        { name: 'Nico Hülkenberg', score: 0.89 },
+        { name: 'Esteban Ocon', score: 0.89 },
+        { name: 'Oliver Bearman', score: 0.85 },
+        { name: 'Sergio Perez', score: 0.90 },
+        { name: 'Valtteri Bottas', score: 0.89 },
     ];
     const cars = [
-        { team: 'Red Bull', score: 0.92 },
+        { team: 'Red Bull', score: 0.96 },
         { team: 'Mercedes', score: 0.95 },
+        { team: 'Ferrari', score: 0.95 },
+        { team: 'McLaren', score: 0.96 },
+        { team: 'Aston Martin', score: 0.92 },
+        { team: 'Alpine', score: 0.88 },
+        { team: 'Williams', score: 0.89 },
+        { team: 'VCARB', score: 0.88 },
+        { team: 'Audi', score: 0.85 },
+        { team: 'Haas', score: 0.86 },
+        { team: 'Cadillac', score: 0.84 },
     ];
     const tires = {
         soft: { base: 1.0, wearRate: 0.015, type: 'soft' },
@@ -123,10 +152,10 @@ let addDriver = () => {
         car: cars[teamIndex],
         strategy: theStrategy
     };
-    
-    if (validator(selectedDrivers,currentDriver)) {
+
+    if (validator(selectedDrivers, currentDriver)) {
         alert("This combination already exists");
-        return; 
+        return;
     }
 
     selectedDrivers.push(currentDriver);
